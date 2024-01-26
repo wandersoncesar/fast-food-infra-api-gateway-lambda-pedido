0f9902e4d5a817d89878a33b8c988924c3269aaa
Wanderson César <contato@wandersoncesar.com.br>
2024-01-26 17:26:19 -0300

    Update main.tf

--- infra/terraform/apigateway/main.tf
+++ infra/terraform/apigateway/main.tf
@@ -3,11 +3,44 @@ resource "aws_api_gateway_rest_api" "api_fast_food" {
   description = "API Gateway para API REST fast-food"
 }
 
+resource "aws_api_gateway_resource" "api_fast_food" {
+  rest_api_id = aws_api_gateway_rest_api.api_fast_food.id
+  parent_id   = aws_api_gateway_rest_api.api_fast_food.root_resource_id
+  path_part   = "api_fast_food"
+}
+
+resource "aws_api_gateway_method" "root_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api_fast_food.id
+  resource_id   = aws_api_gateway_resource.api_fast_food.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "root_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api_fast_food.id
+  resource_id             = aws_api_gateway_resource.api_fast_food.id
+  http_method             = aws_api_gateway_method.root_method.http_method
+  integration_http_method = "GET"
+  type                    = "HTTP_PROXY"
+  uri                     = "http://cliente-alb-fast-food-app-645897645.us-east-1.elb.amazonaws.com"
+}
+
+resource "aws_api_gateway_deployment" "api_fast_food_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api_fast_food.id
+  stage_name  = "dev"
+}
+
+resource "aws_api_gateway_stage" "dev" {
+  stage_name    = "dev"
+  deployment_id = aws_api_gateway_deployment.api_fast_food_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.api_fast_food.id
+}
+
 # /cliente
 
 resource "aws_api_gateway_resource" "cliente" {
   rest_api_id = aws_api_gateway_rest_api.api_fast_food.id
-  parent_id   = aws_api_gateway_rest_api.api_fast_food.root_resource_id
+  parent_id   = aws_api_gateway_resource.api_fast_food.id
   path_part   = "cliente"
 }
 
@@ -53,7 +86,7 @@ resource "aws_api_gateway_integration" "busca_cliente" {
 
 resource "aws_api_gateway_resource" "autenticar" {
   rest_api_id = aws_api_gateway_rest_api.api_fast_food.id
-  parent_id   = aws_api_gateway_rest_api.api_fast_food.root_resource_id
+  parent_id   = aws_api_gateway_resource.api_fast_food.id
   path_part   = "autenticar"
 }
 
@@ -83,15 +116,4 @@ resource "aws_api_gateway_authorizer" "custom" {
   type                             = "REQUEST"
   identity_source                  = "method.request.header.Authorization"
   authorizer_result_ttl_in_seconds = 0
-}
-
-resource "aws_api_gateway_deployment" "api_fast_food_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.api_fast_food.id
-  stage_name  = "dev"
-}
-
-resource "aws_api_gateway_stage" "dev" {
-  stage_name    = "dev"
-  deployment_id = aws_api_gateway_deployment.api_fast_food_deployment.id
-  rest_api_id   = aws_api_gateway_rest_api.api_fast_food.id
 }
